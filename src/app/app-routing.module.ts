63f5c33aa65fcf083749793efb412a071e2d85b1
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-24 07:01:07 +0100

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AwayComponent } from './away/away.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'away', component: AwayComponent}
 ]
 @NgModule({
   imports: [
